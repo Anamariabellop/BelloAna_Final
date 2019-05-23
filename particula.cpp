12842b2750950d30e8142e5ff838ad7d38a26aac
Ana Bello <anamariabellopena@MacBook-Air-de-Ana.local>
2019-05-23 11:35:42 -0500

    archivo c++

--- particula.cpp
+++ particula.cpp
@@ -1,12 +1,71 @@
 #include<iostream>
 #include<cstring>
+#include <fstream>
 
 using namespace std;
 
 float m= 7294.29;
 int q=2;
+float t=0;
+float tfinal= 1000;
+float dt=0.1;
+
 
 
 int main(){
 
+	leapfrog(t,tfinal,dt, "leapfrog.txt")
+	return 0;
+
+}
+
+// mdv/dt = m vn+1 -vn /dt http://www.sc.ehu.es/sbweb/fisica/elecmagnet/movimiento/cicloide/cicloide.htm
+float F(float t, float t_final)
+{
+	return m*a(m ,t , tfinal)	
+}
+
+float dxdt(float t, float t_final)
+{
+	return 0.1;
+}
+
+void a(float m, float t, float tfinal){
+	float E;
+		if( i < 3)
+		{
+			E=3;
+		}
+		elif(i<3)
+		{
+			E=0;
+		}
+
+		elif(i>7)
+		{
+			E=0;
+		}
+
+	return (q*E)/m;
+}
+
+void leapfrog(float t, float t_final, float dt, string name ){
+
+	ofstream outfile;
+	outfile.open()
+
+	float t= 0;
+	float t_final= t_final;
+	float y=0;
+	float x=0;
+
+	while(t < t_final)
+
+	outfile << t  << "\t" << x << "\t" << y << "\t" << endl;
+	y = y + (1/2)*dt*F(m,,t,tfinal);
+	x= x + (1/2)*dt*F(m,,t,tfinal);
+	z= z+ dt*F(m,,t,tfinal);	 
+	t+= dt;
+
+	outfile.close()
 }
